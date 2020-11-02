65aaf6950682fdeb926b26ed39f8aa10657c2fbd
anthonyjasso <anthonyjasso15@gmail.com>
2020-11-01 21:06:30 -0700

    added red25, red50, red75, only_red, only_green, state_one, state_two, siren_state_machine and buzzer_advance to file

--- project_demo/stateMachines.c
+++ project_demo/stateMachines.c
@@ -1,48 +1,113 @@
 #include <msp430.h>
 #include "stateMachines.h"
 #include "led.h"
+#include "buzzer.h"
 
-char toggle_red()		/* always toggle! */
+static char stateCheck = 0;
+static char state      = 0;
+
+void red25()             /* %25 brightness */
 {
-  static char state = 0;
-
   switch (state) {
   case 0:
-    red_on = 1;
-    state = 1;
+    red_on = 0;
+    state  = 1;
     break;
   case 1:
     red_on = 0;
-    state = 0;
+    state  = 2;
+    break;
+  case 2:
+    red_on = 1;
+    state  = 0;
     break;
   }
-  return 1;			/* always changes an led */
-}
-
-char toggle_green()	/* only toggle green if red is on!  */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
-}
-
-
-void state_advance()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
-
-  /*static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
-  }*/
-
-  led_changed = toggle_red();
   led_update();
 }
 
+void red50()		/* %50 brightness */
+{
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state  = 1;
+    break;
+  case 1:
+    red_on = 0;
+    state  = 0;
+    break;
+  }
+  led_update();			/* always changes an led */
+}
 
+void red75()              /* %75 brightness */
+{
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state  = 1;
+    break;
+  case 1:
+    red_on = 1;
+    state  = 2;
+    break;
+  case 2:
+    red_on = 0;
+    state  = 0;
+    break;
+  }
+  led_update();
+}
+    
+void only_red()          /* toggles only red led  */
+{
+  red_on = 1;
+  green_on = 0;
+  led_update();
+}
 
+void only_green()          /* toggles only green led */
+{
+  green_on = 1;
+  red_on   = 0;
+  led_update();
+}
+
+void state_one()          /* states for siren state machine */
+{
+  stateCheck = 1;
+  only_green();
+}
+
+void state_two()
+{
+  stateCheck = 0;
+  only_red();
+}
+
+void siren_state_machine()          /* siren state machine for leds */
+{
+  static char sirenState = 0;
+  switch (sirenState) {
+  case 0:
+  case 1:
+    state_one();
+    sirenState++;
+    break;
+  case 2:
+    state_two();
+    sirenState = 0;
+  default:
+    break;
+  }
+}
+
+void buzzer_advance()          /* buzzer for siren */
+{
+  static int n = 0;
+  if(stateCheck) n = 700;
+  else n = 1000;
+
+  short cycles = 2000000/n;
+  buzzer_set_period(cycles);
+}
